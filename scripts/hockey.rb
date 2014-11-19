61f5c7d0e26369b6ea9aa3f0669996cde04d31e8
Caleb Davenport <calebmdavenport@gmail.com>
2014-11-19 11:28:26 -0800

    Fix upload command.

--- scripts/hockey.rb
+++ scripts/hockey.rb
@@ -21,25 +21,26 @@ def hockey_token
 end
 
 def zip_dsym
-  FileUtils.rm_f dsym_path_after_zip
-  system '/usr/bin/zip', '-r', dsym_path_after_zip, dsym_path_before_zip
+  FileUtils.rm_f(dsym_path_after_zip)
+  system("/usr/bin/zip -r #{dsym_path_after_zip} #{dsym_path_before_zip}")
 end
 
-def upload
-  puts "-H \"X-HockeyAppToken: #{hockey_token}\""
-
-  system(
+def curl_command
+  command = [
     '/usr/bin/curl',
     '-F status=2',
     '-F notify=0',
     "-F ipa=@\"#{ipa_path}\"",
     "-F dsym=@\"#{dsym_path_after_zip}\"",
-    "-H \"X-HockeyAppToken: #{hockey_token}\"",
+    "-H \"X-HockeyAppToken: 70038cfc620f4fe59d7046d7be480ebd\"",
     'https://rink.hockeyapp.net/api/2/apps/upload'
-  )
+  ]
+  command.join(" ")
 end
 
-puts hockey_token
+def upload
+  system(curl_command)
+end
 
 raise 'Unable to create dsym.zip.' unless zip_dsym
 raise 'Upload failed.' unless upload
