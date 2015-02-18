5759fd7c224f9c6326efb0fb4347e0f6a8c7bb27
Caleb Davenport <calebmdavenport@gmail.com>
2015-02-17 21:40:14 -0800

    Add script to remove unwanted localizations.

---
+++ build-scripts/locales.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+SUPPORTED_LOCALES = [ 'Base', 'en' ]
+
+def product_path
+  File.join(ENV['TARGET_BUILD_DIR'], ENV['FULL_PRODUCT_NAME'])
+end
+
+def path_for_locale(locale)
+  File.join(product_path, locale, '.lproj')
+end
+
+def locale_for_path(locale)
+  match = locale.match(/\/(?<locale>[^\/]*)\.lproj\z/)
+  return match[:locale] if match
+  return nil
+end
+
+def compiled_locales
+  pattern = File.join(product_path, '*.lproj')
+  Dir.glob(pattern).inject([]) do |array, path|
+    locale = locale_for_path(path)
+    if locale
+      array + [ locale ]
+    else
+      array
+    end
+  end
+end
+
+compiled_locales.reject do |locale|
+  SUPPORTED_LOCALES.include?(locale)
+end.each do |locale|
+  puts "Removing unsupported locale: #{locale}"
+  path = path_for_locale(locale)
+  `rm -f #{path}`
+end
